042e56aa6289de7c15a2121609055e4d26c58d43
otacisos <otacisods@gmail.com>
2020-11-27 14:27:41 -0300

    Exercicio 9 - Utilizando o array criado no exercício para imprimir o resultado da divisão

---
+++ JSArrayLoopFor/exercises/introducao-a-javascript-e-logica-de-programacao_2/exercise_9.js
@@ -0,0 +1,16 @@
+let array = [];
+
+for (let numbers = 1; numbers <= 25; numbers ++){
+    array.push(numbers);
+
+}
+
+//console.log(array); 
+
+//let lista = [1,  2,  3,  4,  5,  6,  7,  8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25 ];
+
+for (let divisao = 1; divisao <= array.length; divisao ++){
+
+    console.log(array[divisao -1] / 2);
+
+}
