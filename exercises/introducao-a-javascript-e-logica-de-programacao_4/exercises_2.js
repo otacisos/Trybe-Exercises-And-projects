b44335657f572af95299256c213261ef020aa0bc
otacisos <otacisods@gmail.com>
2020-11-30 23:54:47 -0300

    exercises_2.js
    Exercico 2 - Inserir no objeto uma nova propriedade com o nome e chave

--- exercises/introducao-a-javascript-e-logica-de-programacao_4/exercises_2.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_4/exercises_2.js
@@ -4,5 +4,6 @@ let info = {
     nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
     recorrente: 'Sim',
   };
+  
     console.log('Bem vinda' + ' ' + info.personagem + '.' +'\nNão sei se era recorrente, era? ' + info.recorrente);
     console.log(info.recorrente);
