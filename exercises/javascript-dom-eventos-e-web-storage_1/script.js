54ce1e5a5fcbde803af30bc21d7929df5bf97eaf
otacisos <otacisods@gmail.com>
2020-12-04 10:17:07 -0300

    Aualizando Conteudos

--- exercises/javascript-dom-eventos-e-web-storage_1/script.js
+++ exercises/javascript-dom-eventos-e-web-storage_1/script.js
@@ -1,6 +1,8 @@
-console.log("Trybe");
+// console.log("Trybe");
 // Byid v1
-console.log(document.getElementById("teste").innerText);
+// console.log(document.getElementById("teste").innerText);
 
 // Byid v2
-console.log(document.getElementById("teste").innerHTML);
+// console.log(document.getElementById("teste").innerHTML);
+
+console.log(document.getElementById("page-title").innerHTML);
