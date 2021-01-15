060663634343f254d6a32dfa0d6f3448e888c604
otacisos <otacisods@gmail.com>
2021-01-14 22:47:12 -0300

    Exercises_1

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercises_1.js
@@ -0,0 +1,12 @@
+let numA = 5;
+let numB = 2;
+
+console.log(`A soma é: ${numA + numB}`);
+
+console.log(`A subtração é: ${numA - numB}`);
+
+console.log(`A muitlplicação é: ${numA * numB}`);
+
+console.log(`A divi é: ${numA / numB}`);
+
+console.log(`O modulo é: ${numA % numB}`);
