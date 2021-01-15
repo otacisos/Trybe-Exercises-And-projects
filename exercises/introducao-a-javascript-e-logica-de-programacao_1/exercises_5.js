cadd525843da9924a8ffaebaf309073a840f7515
otacisos <otacisods@gmail.com>
2021-01-15 00:32:10 -0300

    Exercises_5

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercises_5.js
@@ -0,0 +1,14 @@
+
+let ang1 = 60;
+let ang2 = 60;
+let ang3 = 60;
+
+let somaAngulos = ang1 + ang2 + ang1;
+
+if (somaAngulos == 180) {
+  console.log('Representam os ângulos de um triângulo ');
+  
+} else {
+  console.log('Não reprçesentam os ângulos de um triângulo ');
+
+}
