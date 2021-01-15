f562ddb5d690aadca8d21b97053efc9491c1ca10
otacisos <otacisods@gmail.com>
2021-01-15 00:47:08 -0300

    Exercises_6

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercises_6.js
@@ -0,0 +1,19 @@
+
+let move = 'tower';
+
+switch (move) {
+  case 'bishop':
+  console.log('diagonals');
+  break;
+
+  case 'tower':
+    console.log('front');
+  break;
+
+  case 'horse':
+    console.log('one-two');
+
+  default:
+    console.log('Digita direito!!');
+    break;
+}
