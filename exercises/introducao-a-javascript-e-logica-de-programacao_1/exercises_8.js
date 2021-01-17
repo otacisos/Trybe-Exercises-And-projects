c40218e745afcba627be642edac46746f2d9eebc
otacisos <otacisods@gmail.com>
2021-01-17 01:17:08 -0300

    Exercises 8

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercises_8.js
@@ -0,0 +1,12 @@
+
+let number1 = 110;
+let number2 = 61;
+let number3 = 141;
+
+let trueF = false;
+
+if ( number1 % 2 === 0 || number2 % 2 === 0 || number3 % 2 === 0) {
+  trueF = true;
+}
+
+console.log(trueF);
