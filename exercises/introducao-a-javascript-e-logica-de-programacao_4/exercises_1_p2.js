6965053df205f00704a95ce8c81fd3c95faad954
otacisos <otacisods@gmail.com>
2021-01-20 19:02:45 -0300

    Exercises_1_p2

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_4/exercises_1_p2.js
@@ -0,0 +1,19 @@
+// Crie uma função que receba uma string e retorne true se for um palíndromo , 
+// ou false , se não for.
+// Exemplo de palíndromo: arara .
+// verificaPalindrome("arara") ;
+// Retorno esperado: true
+// verificaPalindrome("desenvolvimento") ;
+// Retorno esperado: false
+
+// function verificaPalindrome(params) {
+//   for (let i; i< params.length; i++) {
+//       console.log(params[i]);
+//   }
+  
+// } console.log(verificaPalindrome('1, 2, 3') );
+
+let params = 'palavra';
+let letra = '';
+
+ 
