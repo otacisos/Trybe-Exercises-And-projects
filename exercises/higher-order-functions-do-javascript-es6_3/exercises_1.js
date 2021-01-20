3f0e184bc56f0710a7151c499c7b94a0febeed61
otacisos <otacisods@gmail.com>
2021-01-20 15:43:07 -0300

    Exercises_1

---
+++ exercises/higher-order-functions-do-javascript-es6_3/exercises_1.js
@@ -0,0 +1,18 @@
+
+const assert = require('assert');
+
+const arrays = [
+    ["1", "2", "3"],
+    [true],
+    [4, 5, 6]
+];
+
+
+function flatten() {
+  // escreva seu código aqui
+  return arrays.reduce((array, arrays) => array.concat(arrays), []);
+  
+}
+
+
+assert.deepStrictEqual(flatten(), ["1", "2", "3", true, 4, 5, 6]);
