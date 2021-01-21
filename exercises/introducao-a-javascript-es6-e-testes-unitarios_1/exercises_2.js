8b8e1d4bae5f7dd51d3b61fb36e6a83ca3b25f5a
otacisos <otacisods@gmail.com>
2021-01-20 22:22:43 -0300

    Exercises_2

---
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_1/exercises_2.js
@@ -0,0 +1,15 @@
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+const seila2 = () => {
+
+  for (let i = 0; i < oddsAndEvens.length; i++) {
+    oddsAndEvens.sort();
+
+  } return oddsAndEvens;
+  
+} 
+const seila = seila2();
+console.log(seila);
+console.log(`${seila}`);
+
+
