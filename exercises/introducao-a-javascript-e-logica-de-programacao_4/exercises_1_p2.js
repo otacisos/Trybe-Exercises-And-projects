741ab83acbc464f86587d8be870b26646c43ce07
otacisos <otacisods@gmail.com>
2021-01-25 16:19:51 -0300

    Exercises_1 P2

--- exercises/introducao-a-javascript-e-logica-de-programacao_4/exercises_1_p2.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_4/exercises_1_p2.js
@@ -1,4 +1,4 @@
-// Crie uma função que receba uma string e retorne true se for um palíndromo , 
+// Crie uma função que receba uma string e retorne true se for um palíndromo ,
 // ou false , se não for.
 // Exemplo de palíndromo: arara .
 // verificaPalindrome("arara") ;
@@ -10,10 +10,42 @@
 //   for (let i; i< params.length; i++) {
 //       console.log(params[i]);
 //   }
-  
+
 // } console.log(verificaPalindrome('1, 2, 3') );
 
-let params = 'palavra';
-let letra = '';
+//Duas formas de resolver o exercício
 
- 
+function verificaPalin(palavra) {
+  let arrayLetras = palavra.split('');
+  let isPalindrome = true;
+  for (let i = 0; i < arrayLetras.length; i+= 1) {
+    if (arrayLetras[i] != palavra[(palavra.length - 1) - i]) {
+      isPalindrome = false;
+    }
+  }console.log(palavra);
+  return isPalindrome;
+}
+
+// function verificaPalindrome(palavra) {
+//   let arrayLetras = palavra.split("");
+//   let isPalindrome = true;
+//   for (let index in arrayLetras) {
+//     if (arrayLetras[index] != palavra[(palavra.length - 1) - index]) {
+//       isPalindrome = false;
+//     }
+//   }
+//   return isPalindrome;
+// }
+
+// function verificaPalindrome(string) {
+//   let reverse = string.split("").reverse().join("");
+//   if (reverse === string) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+console.log(verificaPalin('arara')); //true
+console.log(verificaPalin('desenvolvimento')); //false
+
